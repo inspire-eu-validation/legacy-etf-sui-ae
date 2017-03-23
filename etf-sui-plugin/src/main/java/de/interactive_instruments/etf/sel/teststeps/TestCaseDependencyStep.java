ce077da39d7981fa33fa6cbc7705d672d53d8cbc
Jon Herrmann <herrmann@interactive-instruments.de>
2017-03-23 11:14:45 +0100

    Dependency test step UI

---
+++ etf-sui-plugin/src/main/java/de/interactive_instruments/etf/sel/teststeps/TestCaseDependencyStep.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright 2010-2016 interactive instruments GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.interactive_instruments.etf.sel.teststeps;
+
+import java.util.List;
+
+import com.eviware.soapui.config.TestStepConfig;
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase;
+import com.eviware.soapui.impl.wsdl.teststeps.WsdlMessageExchangeTestStepResult;
+import com.eviware.soapui.impl.wsdl.teststeps.WsdlRunTestCaseTestStep;
+import com.eviware.soapui.model.testsuite.*;
+import com.eviware.soapui.model.testsuite.TestStepResult.TestStepStatus;
+
+import com.eviware.soapui.plugins.auto.PluginTestStep;
+import de.interactive_instruments.exceptions.ExcUtils;
+
+/**
+ * @author J. Herrmann ( herrmann <aT) interactive-instruments (doT> de )
+ */
+@PluginTestStep(typeName = TestCaseDependencyStepFactory.STEP_ID,
+		name = TestCaseDependencyStepFactory.NAME,
+		description = TestCaseDependencyStepFactory.DESCRIPTION,
+		iconPath = TestCaseDependencyStepFactory.ICON)
+public class TestCaseDependencyStep extends WsdlRunTestCaseTestStep implements TestCaseDependencyTestStepDef {
+
+	public TestCaseDependencyStep(WsdlTestCase testCase, TestStepConfig config, boolean forLoadTest) {
+		super(testCase, config, forLoadTest);
+	}
+
+	public TestStepResult run(TestCaseRunner testRunner, TestCaseRunContext testRunContext) {
+		final Object o = testRunner.getRunContext().getProperty("#ProjectRunner#");
+		if (o != null) {
+			try {
+				final ProjectRunner projectRunner = (ProjectRunner) o;
+				// Check if Test Case has already be run
+				final String testSuiteId = getTargetTestCase().getParent().getId();
+				final List<TestSuiteRunner> results = projectRunner.getResults();
+				for (final TestSuiteRunner testSuiteRunner : results) {
+					if (testSuiteRunner.getTestSuite().getId().equals(testSuiteId)) {
+						for (final TestCaseRunner caseRunner : testSuiteRunner.getResults()) {
+							if (caseRunner.getTestCase().getId().equals(getTargetTestCase().getId())) {
+								if(caseRunner.getStatus() == TestRunner.Status.RUNNING) {
+									caseRunner.waitUntilFinished();
+								}
+								final WsdlMessageExchangeTestStepResult result = new WsdlMessageExchangeTestStepResult(this);
+								result.setTimeStamp(System.currentTimeMillis());
+								result.setTimeTaken(caseRunner.getTimeTaken());
+								result.setStatus(runnerStatusToTestStepStatus(caseRunner));
+								return result;
+							}
+						}
+					}
+				}
+			} catch (final ClassCastException ign) {
+				ExcUtils.suppress(ign);
+			}
+		}
+		return super.run(testRunner, testRunContext);
+	}
+
+	private TestStepStatus runnerStatusToTestStepStatus(final TestCaseRunner testCaseRunner) {
+		switch (testCaseRunner.getStatus()) {
+		case CANCELED:
+			return TestStepStatus.CANCELED;
+		case FAILED:
+			return TestStepStatus.FAILED;
+		case FINISHED:
+			return TestStepStatus.OK;
+		default:
+			return TestStepStatus.UNKNOWN;
+		}
+	}
+}
---
+++ etf-sui-plugin/src/main/java/de/interactive_instruments/etf/sel/teststeps/TestCaseDependencyStepFactory.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2010-2016 interactive instruments GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.interactive_instruments.etf.sel.teststeps;
+
+import com.eviware.soapui.config.TestStepConfig;
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase;
+import com.eviware.soapui.impl.wsdl.teststeps.WsdlTestStep;
+import com.eviware.soapui.impl.wsdl.teststeps.registry.WsdlTestStepFactory;
+
+/**
+ * @author J. Herrmann ( herrmann <aT) interactive-instruments (doT> de )
+ */
+public class TestCaseDependencyStepFactory extends WsdlTestStepFactory {
+
+	public static final String STEP_ID = "testcasedependency";
+	public static final String NAME = "Test Case Dependency";
+	public static final String DESCRIPTION = "Executes a Test Case, if it hasn't been already executed in a Test Run, "
+			+ "and fails if the dependent Test Case fails";
+	public static final String ICON = "/run_test_case_step.png";
+
+	public TestCaseDependencyStepFactory() {
+		super(STEP_ID, NAME,
+				DESCRIPTION, null);
+	}
+
+	@Override
+	public WsdlTestStep buildTestStep(final WsdlTestCase wsdlTestCase, final TestStepConfig testStepConfig, final boolean forLoadTest) {
+		return new TestCaseDependencyStep(wsdlTestCase, testStepConfig, forLoadTest);
+	}
+
+	@Override
+	public TestStepConfig createNewTestStep(final WsdlTestCase wsdlTestCase, final String s) {
+		TestStepConfig testStepConfig = TestStepConfig.Factory.newInstance();
+		testStepConfig.setType(STEP_ID);
+		testStepConfig.setName(getTestStepName());
+		return testStepConfig;
+	}
+
+	@Override
+	public boolean canCreate() {
+		return true;
+	}
+}
